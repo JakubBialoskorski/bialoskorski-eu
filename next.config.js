24159463cfd5b2d618811a032bf19680df6f44c0
Jakub Białoskórski <bialoskorski@icloud.com>
2023-01-21 22:06:16 +0100

    revert to original settings without domain

--- next.config.js
+++ next.config.js
@@ -1,20 +1,18 @@
-// const isGithubActions = process.env.GITHUB_ACTIONS || false
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-// let assetPrefix = ''
-// let basePath = '/'
+let assetPrefix = ''
+let basePath = '/'
 
-// if (isGithubActions) {
-//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-//   assetPrefix = `/${repo}/`
-//   basePath = `/${repo}`
-// }
-
-
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 module.exports = {
-  basePath: process.env.NODE_ENV === "production" ? "/bialoskorski.eu" : undefined,
-  assetPrefix: process.env.NODE_ENV === "production" ? "/bialoskorski.eu" : undefined,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     loader: 'imgix',
     path: 'bialoskorski.imgix.net',
