81b28f59e56ff265b1b4c8444772a18f49ea541b
Jakub Białoskórski <bialoskorski@icloud.com>
2023-01-22 09:19:41 +0100

    add basic cookie consent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from 'next/link'
+import CookieConsent from "react-cookie-consent"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <CookieConsent>This website uses <Link href="https://www.cloudflare.com/learning/privacy/what-are-cookies/">cookies</Link> to enhance the user experience.</CookieConsent>
     </>
   ) 
 }
