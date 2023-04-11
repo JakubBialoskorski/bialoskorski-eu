0b5ae83b1b18a844c947384e32a4f226f65bd6a6
Jakub Białoskórski <bialoskorski@icloud.com>
2023-04-11 17:38:54 +0200

    exclude projects from sitemap and robots files

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,12 @@ const siteUrl = "https://bialoskorski.eu";
 module.exports = {
     siteUrl,
     generateRobotsTxt: true,
-    exclude: ["/projects/*"],
+    robotsTxtOptions: {
+        policies: [
+            { userAgent: "*", disallow: "/projects" },
+            { userAgent: "*", disallow: "/projects/*" },
+            { userAgent: "*", allow: "/" },
+        ],
+    },
+    exclude: ["/projects", "/projects/*"],
 };
