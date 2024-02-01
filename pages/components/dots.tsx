e4700b9d5d1d57df0eda90d0e5a2ce3ff4f62225
AgataBialoskorska <agatamirk@gmail.com>
2024-02-01 13:51:59 +0100

    change index behavior

--- pages/components/dots.tsx
+++ pages/components/dots.tsx
@@ -1,34 +1,23 @@
 import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 
 function Dots() {
-  const router = useRouter()
+	useEffect(() => {
+		let menuEl: any
+		menuEl = document.querySelector('.menu')
+		const menuItems = document.querySelectorAll('.menu-item')
 
-  useEffect(() => {
-    let menu
-    menu = document.querySelector('.menu')
-    const menuItems = document.querySelectorAll('.menu-item')
+		const handleMouseOver = index => {
+			menuEl.dataset.activeIndex = index
+		}
 
-    const handleMouseOver = (index) => {
-      menu.dataset.activeIndex = index
-    }
-
-    const handleMouseOut = () => {
-      menu.dataset.activeIndex = ''
-    }
-
-    Array.from(menuItems).forEach((item, index) => {
-      const element = item as HTMLElement
-      element.addEventListener('mouseover', () => {
-        handleMouseOver(index)
-      })
-      element.addEventListener('mouseout', () => {
-        handleMouseOut()
-      })
-    })
-  }, [router])
-
-  return 
+		Array.from(menuItems).forEach((item, index) => {
+			const element = item as HTMLElement
+			element.addEventListener('mouseover', () => {
+				handleMouseOver(index)
+			})
+		})
+	})
+	return
 }
 
-export default Dots;
+export default Dots
