a6da8931d053133a254e38b284fc54a6c8134c50
Jakub Białoskórski <bialoskorski@icloud.com>
2024-05-13 10:41:31 +0200

    further adjust new configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
-// Below configuration required by next 14.x
-
-const nextConfig = {
-  output: 'export',
+module.exports = {
   images: {
     loader: 'imgix',
     path: 'bialoskorski.imgix.net',
+  },
+  // New output configuration for Next.js 14.x
+  output: {
+    // Set your export settings here
+    // For example:
+    // export: {
+    //   path: 'your-custom-export-path',
+    //   // Other export options
+    // }
   }
 }
-
-module.exports = nextConfig
