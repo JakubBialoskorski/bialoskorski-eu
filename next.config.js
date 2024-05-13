9f77c65afffeaa2781b07a41657689703838c06f
Jakub Białoskórski <bialoskorski@icloud.com>
2024-05-13 10:36:28 +0200

    fix typo

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const { default: next } = require('next')
-
 // Below configuration required by next 14.x
 
-**
- * @type {import('next').NextConfig}
- */
 const nextConfig = {
   output: 'export',
   images: {
