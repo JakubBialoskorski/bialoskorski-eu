2e93c9b538fb9e9020d0f905863b91a1e39a4d63
Jakub Białoskórski <bialoskorski@icloud.com>
2026-06-17 19:43:45 +0200

    fix cypress baseUrl not actually being set

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,14 +2,6 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      'http://localhost:3000'
-    },
-  },
-  component: {
-    devServer: {
-      framework: 'next',
-      bundler: 'webpack',
-    },
+    baseUrl: 'http://localhost:3000',
   },
 });
