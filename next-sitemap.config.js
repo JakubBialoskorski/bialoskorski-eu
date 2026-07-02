32cbd09b5245555fd2cde3fb975551dfb5f0499e
Jakub Białoskórski <bialoskorski@icloud.com>
2026-07-02 09:44:19 +0200

    remove stale /projects rules from sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,12 +3,4 @@ const siteUrl = "https://bialoskorski.eu";
 module.exports = {
     siteUrl,
     generateRobotsTxt: true,
-    robotsTxtOptions: {
-        policies: [
-            { userAgent: "*", disallow: "/projects" },
-            { userAgent: "*", disallow: "/projects/*" },
-            { userAgent: "*", allow: "/" },
-        ],
-    },
-    exclude: ["/projects", "/projects/*"],
 };
